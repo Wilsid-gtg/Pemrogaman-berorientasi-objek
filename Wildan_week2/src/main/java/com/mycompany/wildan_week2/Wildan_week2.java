b3ebe848292939fb307d6a207d3b67aed34b0e9d
wildan-gtg <sidqiwildan61@gmail.com>
2024-08-30 21:03:08 +0700

    tugas 2

---
+++ Wildan_week2/src/main/java/com/mycompany/wildan_week2/Wildan_week2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wildan_week2;
+
+/**
+ *
+ * @author Inspiron
+ */
+public class Wildan_week2 {
+
+    public static void main(String[] args) {
+        System.out.println("Tugas 1");
+        String nama = "Muhammad Wildan Sidqi";
+        String npm = "2340506074";
+        String alamat = "Payaman, Secang, Magelang";
+        String noHp = "0895 2713 9053";
+
+        System.out.println("- Nama   : " + nama);
+        System.out.println("- NPM    : " + npm);
+        System.out.println("- Alamat : " + alamat);
+        System.out.println("- No_HP  : " + noHp);
+    }
+}
