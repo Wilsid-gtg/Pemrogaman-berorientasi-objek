1ace938e62b0428851c1f95a599184f51bc4982b
wildan-gtg <sidqiwildan61@gmail.com>
2024-08-30 21:34:09 +0700

    Week 2

---
+++ week2/Wildan_proyek1.java
@@ -0,0 +1,23 @@
+/*
+
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Wildan_proyek1 {
+
+    public static void main(String[] args) {
+        System.out.println("Tugas 1");
+        String nama = "Muhammad Wildan Sidqi";
+        String npm = "2340506074";
+        String alamat = "Payaman, Secang, Magelang";
+        String noHp = "0895 2713 9053";
+
+        System.out.println("- Nama   : " + nama);
+        System.out.println("- NPM    : " + npm);
+        System.out.println("- Alamat : " + alamat);
+        System.out.println("- No_HP  : " + noHp);
+    }
+}
