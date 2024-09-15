9933a1ca41f52578807f877f81880e8cbc1aac6b
wildan-gtg <sidqiwildan61@gmail.com>
2024-09-15 13:27:50 +0700

    praktikum 4

---
+++ prak_4/src/main/java/prak_4/Main.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prak_4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+    public static void main(String[] args) {
+        pekerja orang1 = new pekerja("Paijo",28,"Joki",5000000);
+        
+        System.out.println("-- "+orang1.getNama()+" --");
+        orang1.OverridemetodetoString();
+        
+        System.out.println();
+        
+        orang1.setNama("Bambang");
+        orang1.setGaji(20000);
+        System.out.println("-- "+orang1.getNama()+" --");
+        orang1.OverridemetodetoString();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
